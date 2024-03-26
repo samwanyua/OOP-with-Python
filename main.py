873ca88e24cfce86af9ab6ba5df31a4bca709dbd
samwanyua <samexample8@gmail.com>
2024-03-27 00:05:36 +0300

    Implemented __init__ magic method and added validations on the arguments

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 class Item:
     # constructor
-    def __init__(self,name, price, quantity): 
+    def __init__(self,name: str, price: float, quantity = 0): 
+        # Validations to recieved arguments
+        assert price >= 0, f"Price {price} is not greater than  or equal to zero"
+        assert quantity >= 0, f"Price {quantity} is not greater than  or equal to  zero"
+
+        # assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+
         
-    def total_price(self, x, y):
-        return x * y
+    def total_price(self):
+        return self.price * self.quantity
 
-item1 = Item("Phone", 12000,5)
-
-print(item1.total_price(item1.price,item1.quantity ))
-
+item1 = Item("Phone", 12000, 5)
 item2 = Item("Laptop",340000, 23)
-print(item2.total_price(item2.price,item2.quantity ))
+
+print(item1.total_price())
+print(item2.total_price())
 
 
 

50e58c1b2f1e9063964c38e8a5f3f0c961a1f2d2
samwanyua <samexample8@gmail.com>
2024-03-27 05:28:34 +0300

    Implemented class attributes and instance attributes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:
+    # class attribute
+    pay_rate = 0.8 # pay rate after 20% discount
     # constructor
     def __init__(self,name: str, price: float, quantity = 0): 
         # Validations to recieved arguments
@@ -10,15 +12,31 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-        
     def total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
 item1 = Item("Phone", 12000, 5)
 item2 = Item("Laptop",340000, 23)
 
 print(item1.total_price())
 print(item2.total_price())
+
+print(Item.pay_rate)
+print(item1.pay_rate)
+
+print(Item.__dict__) #atrributes for class level
+print(item1.__dict__) #atrributes for instance level
+
+item1.apply_discount()
+print(item1.price)
+
+# diff discount rate
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
 
 
 

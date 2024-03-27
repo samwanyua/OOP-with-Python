368e5e8bb2f97b91471392aec7c8944f1836cc2a
samwanyua <samexample8@gmail.com>
2024-03-27 05:43:42 +0300

    Representing objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Item:
     # class attribute
     pay_rate = 0.8 # pay rate after 20% discount
+    all = []
     # constructor
     def __init__(self,name: str, price: float, quantity = 0): 
         # Validations to recieved arguments
@@ -12,31 +13,51 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # actions to execute
+        Item.all.append(self)
+
     def total_price(self):
         return self.price * self.quantity
     
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
 item1 = Item("Phone", 12000, 5)
 item2 = Item("Laptop",340000, 23)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-print(item1.total_price())
-print(item2.total_price())
+print(Item.all)
+# for instance in Item.all:
+#     print(instance.name)
 
-print(Item.pay_rate)
-print(item1.pay_rate)
 
-print(Item.__dict__) #atrributes for class level
-print(item1.__dict__) #atrributes for instance level
 
-item1.apply_discount()
-print(item1.price)
 
-# diff discount rate
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+
+
+
+
+# print(item1.total_price())
+# print(item2.total_price())
+
+# print(Item.pay_rate)
+# print(item1.pay_rate)
+
+# print(Item.__dict__) #atrributes for class level
+# print(item1.__dict__) #atrributes for instance level
+
+# item1.apply_discount()
+# print(item1.price)
+
+# # diff discount rate
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+# print(item2.price)
 
 
 
